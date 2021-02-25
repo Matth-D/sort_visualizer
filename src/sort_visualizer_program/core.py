84cb715063526c2fedd2eff21ddc8ea13a815c23
Matthieu Druaud <matthieu.druaud@gmail.com>
2021-02-25 21:47:31 +0000

    setting up project

--- src/sort_visualizer_program/core.py
+++ src/sort_visualizer_program/core.py
@@ -1,5 +1,11 @@
 import numpy as np
 
 
-def create_array_random(length):
+def create_array_random(array_length):
+
+    if not isinstance(array_length, int):
+        print("Length parameter must be integer")
+        return
+    array = np.random.rand(array_length)
+    return array
 
