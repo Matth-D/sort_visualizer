ea2ce9981638d048ad3ea9a2a9c5c3e4a2253139
Matth-D <matthieu.druaud@gmail.com>
2021-02-27 10:30:31 +0000

    rename and start merge sort

---
+++ src/sort_viz_program/algorithms/merge_sort.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class MergeSort:
+    def __init__(self, sort_array):
+        self.time_complexity = "O(nlogn)"
+        self.space_complexity = "O(n)"
+        self.sort_array = sort_array
+        self.inf = float("inf")
+
+    def solve(self):
+        array_len = len(self.sort_array)
+
+
+my_array = np.random.rand(6)
+algo = MergeSort(my_array)
+algo.solve()
