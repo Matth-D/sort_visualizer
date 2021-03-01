231cd0e05e59181cc0b447681657351469167c8b
Matthieu Druaud <matthieu.druaud@gmail.com>
2021-03-01 22:38:33 +0000

    /

--- src/sort_viz_program/ui.py
+++ src/sort_viz_program/ui.py
@@ -17,11 +17,57 @@ class SortVisualizer(QtWidgets.QDialog):
             round(self.screen_size.width() * 0.4),
             round(self.screen_size.height() * 0.5),
         )
+        # Create Widgets
         self.main_layout = QtWidgets.QVBoxLayout()
+
         self.array_viewer = QtWidgets.QWidget()
+        self.array_viewer.setStyleSheet("background-color:teal")
         self.array_length_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
-        self.layout_h1 = QtWidgets.QHBoxLayout(self.main_layout)
+
+        self.layout_h1 = QtWidgets.QHBoxLayout()
+
         self.layout_v1 = QtWidgets.QVBoxLayout()
+        self.algorithm_list = QtWidgets.QComboBox()
+        self.vertical_spacer1 = QtWidgets.QSpacerItem(
+            20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding
+        )
+
+        self.layout_v2 = QtWidgets.QVBoxLayout()
+
+        self.layout_h2 = QtWidgets.QHBoxLayout()
+
+        self.sort_button = QtWidgets.QPushButton("Sort", self)
+        self.reset_button = QtWidgets.QPushButton("Reset", self)
+
+        self.layout_v3 = QtWidgets.QVBoxLayout()
+        self.infos_label = QtWidgets.QLabel("Infos:")
+        self.array_length_label = QtWidgets.QLabel("Array Length:")
+        self.iterations_label = QtWidgets.QLabel("Iterations:")
+        self.time_complexity_label = QtWidgets.QLabel("Time Complexity:")
+        self.space_complexity_label = QtWidgets.QLabel("Space Complexity:")
+
+        # TODO: Figure the stretching shite
+        # Populate Layout
+        self.setLayout(self.main_layout)
+        self.main_layout.addWidget(self.array_viewer)
+        self.main_layout.addWidget(self.array_length_slider)
+        self.main_layout.addLayout(self.layout_h1)
+
+        self.layout_h1.addLayout(self.layout_v1)
+        self.layout_v1.addWidget(self.algorithm_list)
+        # self.layout_v1.addItem(self.vertical_spacer1)
+        self.layout_h1.addLayout(self.layout_v2)
+        self.layout_v2.addLayout(self.layout_h2)
+
+        self.layout_h2.addWidget(self.sort_button)
+        self.layout_h2.addWidget(self.reset_button)
+
+        self.layout_h1.addLayout(self.layout_v3)
+        self.layout_v3.addWidget(self.infos_label)
+        self.layout_v3.addWidget(self.array_length_label)
+        self.layout_v3.addWidget(self.iterations_label)
+        self.layout_v3.addWidget(self.time_complexity_label)
+        self.layout_v3.addWidget(self.space_complexity_label)
 
     def center_window(self):
         """Centers window on screen."""
