7fec68407774c6a76a29be98a6ee2b9123ab4a80
Matthieu Druaud <matthieu.druaud@gmail.com>
2021-03-09 22:48:33 +0000

    /

--- src/sort_viz_program/algorithms/top_down_merge_sort.py
+++ src/sort_viz_program/algorithms/top_down_merge_sort.py
@@ -41,41 +41,44 @@ class MergeSort:
 
     def merge(self, left, right):
         merged_array = np.empty((0, 2))
+
+        left_index = int(left[0][0])
+        right_index = int(right[-1][0])
+
+        slice_array = np.vstack((left, right))
+
         while left.size and right.size:
             if left[0][1] >= right[0][1]:
                 merged_array = np.vstack((merged_array, right[0]))
                 right = np.delete(right, 0, axis=0)
+                slice_array[0 : len(merged_array)] = merged_array
                 continue
             if left[0][1] < right[0][1]:
                 merged_array = np.vstack((merged_array, left[0]))
                 left = np.delete(left, 0, axis=0)
+                slice_array[0 : len(merged_array)] = merged_array
                 continue
         if not left.size:
             merged_array = np.vstack((merged_array, right))
+            slice_array[0 : len(merged_array)] = merged_array
         if not right.size:
             merged_array = np.vstack((merged_array, left))
+            slice_array[0 : len(merged_array)] = merged_array
+        print(slice_array)
+        print("----")
+        # print(self.sort_array[left_index:right_index])
+        # print("merged:", merged_array)
+        # print("----")
         return merged_array
 
 
-# arr1 = np.array([3, 2, 8, 1, 2, 9, 1])
-arr1 = core.create_array_random(10)
-# empty = np.empty((0, 2))
-# extract1 = arr1[0]
-# extract2 = arr1[1]
-# empty = np.vstack((empty, extract1))
-# empty = np.vstack((empty, extract2))
-# empty = np.vstack((empty, extract2))
-# print(empty)
+index = np.array(range(7))
+values = np.array([3, 2, 8, 1, 2, 9, 1])
+arr1 = np.column_stack((index, values))
 
-# print(empty)
-# result = np.append(empty, extract)
-
-print(arr1)
 algo = MergeSort(arr1)
 algo.solve()
-result = algo.sort_array
-print(result)
-
+# result = algo.sort_array
 
 # backup merge
 # def merge(self, left, right):
@@ -94,3 +97,11 @@ print(result)
 #     if not right.size:
 #         merged_array = np.append(merged_array, left)
 #     return merged_array
+
+# list1 = np.array([[1, 2], [3, 4]])
+# list2 = np.array([[8, 9], [1, 1]])
+
+# temp_array = np.vstack((list1, list2))
+# # temp_arra
+# list1[0 : len(list2)] = list2
+# print(list1)
