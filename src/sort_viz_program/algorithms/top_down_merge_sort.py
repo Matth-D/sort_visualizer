9e9a45ec0609c47f53d0d476bdd7aef281ff3477
Matthieu Druaud <matthieu.druaud@gmail.com>
2021-03-12 18:50:28 +0000

    /

--- src/sort_viz_program/algorithms/top_down_merge_sort.py
+++ src/sort_viz_program/algorithms/top_down_merge_sort.py
@@ -9,11 +9,6 @@ if core_path not in sys.path:
 
 import sort_viz_program.core as core
 
-# from . import algo_utils
-
-
-# print(algo_utils)
-
 
 class MergeSort:
     def __init__(self, input_array):
@@ -40,11 +35,19 @@ class MergeSort:
 
         return self.merge(array_left, array_right)
 
-    # def update_sort_array(left, right, merged_array):
-    #     left_index = int(left[0][0])
-    #     right_index = int(right[-1][0])
+    def update_sort_array(self, left, left_index, right, right_index, merged_array):
+        left_index = int(left[0][0])
+        right_index = int(right[-1][0])
 
-    #     slice_array = np.vstack((left, right))
+        slice_array = np.vstack((left, right))
+
+        slice_array[0 : len(merged_array)] = merged_array
+        slice_array[:, 0] = np.arange(left_index, right_index + 1)
+
+        self.sort_array[left_index : right_index + 1] = slice_array
+        self.sort_array[:, 0] = np.arange(len(self.sort_array))
+
+        print(self.sort_array)
 
     def merge(self, left, right):
         merged_array = np.empty((0, 2))
@@ -57,10 +60,10 @@ class MergeSort:
             if left[0][1] >= right[0][1]:
                 merged_array = np.vstack((merged_array, right[0]))
                 right = np.delete(right, 0, axis=0)
-                slice_array[0 : len(merged_array)] = merged_array
-                slice_array[:, 0] = np.arange(left_index, right_index + 1)
-                self.sort_array[left_index : right_index + 1] = slice_array
-                self.sort_array[:, 0] = np.arange(len(self.sort_array))
+                # slice_array[0 : len(merged_array)] = merged_array
+                # slice_array[:, 0] = np.arange(left_index, right_index + 1)
+                # self.sort_array[left_index : right_index + 1] = slice_array
+                # self.sort_array[:, 0] = np.arange(len(self.sort_array))
                 continue
             if left[0][1] < right[0][1]:
                 merged_array = np.vstack((merged_array, left[0]))
