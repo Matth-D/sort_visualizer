7ebfa95490255510eed08c767d5a82214305d866
Matthieu Druaud <matthieu.druaud@gmail.com>
2021-04-06 22:52:31 +0100

    /

---
+++ src/sort_viz_program/algorithms/heap_sort.py
@@ -0,0 +1,11 @@
+# TODO: as training build on what I've learned tonight and recreate the maxheapify function on a simple array
+
+
+input_array = [5, 91, 12, 8, 57, 35, 43, 26, 89]
+
+# PSEUDOCODE
+
+# GET ARRAY LENGTH
+# START VALUE IS LEN(A) / 2 - 1
+# AND ITERATE DOWN BY -1 TO ITERATE ON EACH SUBTREE ROOT
+# EACH HEAPIFY ITERATION TRICKLES DOWN TO THE LARGEST ESTABLISHED NODE
