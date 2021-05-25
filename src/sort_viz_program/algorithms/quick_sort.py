c2627bb356a7874b0a2703abcf6c6d001666abec
Matth-D <matthieu.druaud@gmail.com>
2021-05-26 00:21:48 +0100

    quicssort

--- src/sort_viz_program/algorithms/quick_sort.py
+++ src/sort_viz_program/algorithms/quick_sort.py
@@ -11,3 +11,45 @@ def quicksort(input_array):
 
 array = [1, 18, 91, 32, 1, 1, 9, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]
 print(quicksort(array))
+
+
+import numpy as np
+
+from . import algo_utils
+
+
+class MergeSort:
+    """Merge sort algorithm."""
+
+    def __init__(self, input_array, **kwargs):
+        """Class init.
+
+        Args:
+            input_array (np.ndarray): Array to sort.
+        """
+        self.gui = kwargs.get("gui")
+        self.signals = algo_utils.Signals()
+        self.time_complexity = "O(nlogn)"
+        self.space_complexity = "O(n)"
+        self.input_array = input_array
+        tmp_index = np.arange(len(self.input_array))
+        self.input_array = np.column_stack((tmp_index, self.input_array))
+        self.sort_array = self.input_array.copy()
+        self.solving = 0
+        self.solved = 0
+        self.iterations = 0
+
+    def solve(self):
+        """Solve Merge sort algorithm."""
+        if self.solving == 1:
+            return
+
+        if self.solved == 1:
+            return
+
+        self.solving = 1
+
+        # self.input_array = self.divide(self.input_array)
+
+        self.solving = 0
+        self.solved = 1
