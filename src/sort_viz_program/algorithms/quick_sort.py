ec19764ccd4908196839399392b7772243dee1bd
Matthieu Druaud <matthieu.druaud@gmail.com>
2021-05-27 00:05:45 +0100

    quicksort

--- src/sort_viz_program/algorithms/quick_sort.py
+++ src/sort_viz_program/algorithms/quick_sort.py
@@ -5,21 +5,22 @@ def quicksort(input_array):
     pivot = input_array[0]
     smaller = [i for i in input_array[1:] if i < pivot]
     greater = [i for i in input_array[1:] if i >= pivot]
-
+    print(quicksort(smaller) + [pivot] + quicksort(greater))
     return quicksort(smaller) + [pivot] + quicksort(greater)
 
 
-array = [1, 18, 91, 32, 1, 1, 9, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]
-print(quicksort(array))
+# array = [1, 18, 91, 32, 1, 1, 9, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]
+array = [5, 2, 6, 1, 8]
 
+# quicksort(array)
 
 import numpy as np
 
-from . import algo_utils
+# from . import algo_utils
 
 
-class MergeSort:
-    """Merge sort algorithm."""
+class QuickSort:
+    """QuickSort algorithm."""
 
     def __init__(self, input_array, **kwargs):
         """Class init.
@@ -27,8 +28,8 @@ class MergeSort:
         Args:
             input_array (np.ndarray): Array to sort.
         """
-        self.gui = kwargs.get("gui")
-        self.signals = algo_utils.Signals()
+        # self.gui = kwargs.get("gui")
+        # self.signals = algo_utils.Signals()
         self.time_complexity = "O(nlogn)"
         self.space_complexity = "O(n)"
         self.input_array = input_array
@@ -49,7 +50,28 @@ class MergeSort:
 
         self.solving = 1
 
-        # self.input_array = self.divide(self.input_array)
+        self.quicksort(self.input_array)
 
         self.solving = 0
         self.solved = 1
+
+    def quicksort(self, input_array):
+        if len(input_array) < 2:
+            return input_array
+
+        pivot = input_array[0]
+        smaller = input_array[input_array[1] < pivot[1]]
+        print(pivot)
+        print(smaller)
+
+        # return np.hstack((self.quicksort(smaller), pivot, self.quicksort(greater)))
+
+
+array = np.array([5, 1, 2, 9, 6])
+selection = array[array > 4]
+algo = QuickSort(array)
+algo.solve()
+#
+# arr1 = np.array([3, 4])
+# arr2 = np.array([1, 2])
+
